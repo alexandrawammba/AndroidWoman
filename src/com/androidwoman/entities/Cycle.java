a5010e23f1bf4d9eed8b4fc774046b076e164f40
rample <alexandrawamba@gmail.com>
2016-11-20 18:32:37 +0100

    Classes des entities

---
+++ src/com/androidwoman/entities/Cycle.java
@@ -0,0 +1,12 @@
+package com.androidwoman.entities;
+
+public class Cycle {
+
+	private int id_cycle;
+	private String begDate;
+	private String finDate;
+	private String begPeriode;
+	private String endPeriode;
+	
+	
+}
